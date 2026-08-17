577ef477b22ea40886eeb2c36876200efc93ea46
Muhammad-Moiz-Latif <moizlatifm@gmail.com>
2026-08-17 14:55:59 +0500

    some build changes to srver now

--- server/src/redis/client.ts
+++ server/src/redis/client.ts
@@ -1,15 +1,14 @@
-// src/lib/redis.ts
 import Redis from 'ioredis'
 import 'dotenv/config'
 import { type ConnectionOptions } from 'bullmq'
 
+const redisUrl = process.env.REDIS_URL;
 const redisHost = process.env.REDIS_HOST || '127.0.0.1'
 const redisPort = Number(process.env.REDIS_PORT) || 6379
 
-export const redisClient = new Redis({
-    host: redisHost,
-    port: redisPort,
-})
+export const redisClient = redisUrl
+    ? new Redis(redisUrl)
+    : new Redis({ host: redisHost, port: redisPort })
 
 redisClient.on('connect', () => {
     console.log('Redis connected')
@@ -19,9 +18,16 @@ redisClient.on('error', (err) => {
     console.error('Redis error:', err)
 })
 
-export const redisConnection: ConnectionOptions = {
-    host: redisHost,
-    port: redisPort,
-    maxRetriesPerRequest: null,
-    enableReadyCheck: false,
-}
+export const redisConnection: ConnectionOptions = redisUrl
+    ? {
+        host: new URL(redisUrl).hostname,
+        port: Number(new URL(redisUrl).port),
+        maxRetriesPerRequest: null,
+        enableReadyCheck: false,
+    }
+    : {
+        host: redisHost,
+        port: redisPort,
+        maxRetriesPerRequest: null,
+        enableReadyCheck: false,
+    }
